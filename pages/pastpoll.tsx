822932cb644fa9808872182b8a4ea47249a891a3
nahid-hasan <nahid.hasan3194@gmail.com>
2022-12-26 08:33:06 +0600

    adjust page add and modify other page

--- pages/pastpoll.tsx
+++ pages/pastpoll.tsx
@@ -6,7 +6,7 @@ export default function pastpoll() {
       <Header />
       <div className="flex-auto">
         <div className="max-w-3xl mx-auto pt-10 pb-12 px-8">
-          <div className="bg-white mt-8 px-4 mobile:p-6 py-5 border-t-4 border-orange-300 overflow-visible rounded-md border-x border border-[#e5e7eb] border-t-[#0a0a2f]">
+          <div className="bg-white mt-8 mobile:p-6 p-5 border-t-4 border-orange-300 overflow-visible rounded-md border-x border border-[#e5e7eb] border-t-[#0a0a2f]">
             <div className="poll-title mb-4">
               <h1 className="text-2xl font-bold leading-7 mobile:text-3xl mobile:truncate text-[#0A0A2F] dark:text-gray-100">
                 Past Polls
@@ -14,9 +14,9 @@ export default function pastpoll() {
               <p className="mt-1 text-sm text-[#0A0A2F]">Below are the Past Polls create by the administrators:</p>
             </div>
 
-            <ul className="max-w-2xl mx-auto p-2  rounded-md border border-[#e5e7eb] cursor-pointer">
+            <ul className="max-w-2xl mx-auto p-4  rounded-md border border-[#e5e7eb] cursor-pointer">
               <li className="text-base text-[#0A0A2F] mobile:text-lg font-bold">CEO Election</li>
-              <li className="text-right mobile:text-left mobile:text-xs text-sm py-1">User Voter : 1205</li>
+              <li className="text-right mobile:text-left mobile:text-xs text-sm">User Voter : 1205</li>
               <li>
                 <ul className="space-x-20 content-center">
                   <li className="flex text-[#079D0D] text-sm mt-1">
@@ -27,9 +27,9 @@ export default function pastpoll() {
               </li>
             </ul>
 
-            <ul className="max-w-2xl mx-auto p-2  rounded-md border border-[#e5e7eb] cursor-pointer mt-4">
+            <ul className="max-w-2xl mx-auto p-4 rounded-md border border-[#e5e7eb] cursor-pointer mt-4">
               <li className="text-base text-[#0A0A2F] mobile:text-lg font-bold">Manager Election</li>
-              <li className="text-right mobile:text-left mobile:text-xs text-sm py-1">User Voter : 1250</li>
+              <li className="text-right mobile:text-left mobile:text-xs text-sm">User Voter : 1250</li>
               <li>
                 <ul className="space-x-20 content-center">
                   <li className="flex text-[#079D0D] text-sm mt-1">
@@ -40,9 +40,9 @@ export default function pastpoll() {
               </li>
             </ul>
 
-            <ul className="max-w-2xl mx-auto p-2  rounded-md border border-[#e5e7eb] cursor-pointer mt-4">
+            <ul className="max-w-2xl mx-auto p-4 rounded-md border border-[#e5e7eb] cursor-pointer mt-4">
               <li className="text-base text-[#0A0A2F] mobile:text-lg font-bold">MD Election</li>
-              <li className="text-right mobile:text-left mobile:text-xs text-sm py-1">User Voter : 1850</li>
+              <li className="text-right mobile:text-left mobile:text-xs text-sm">User Voter : 1850</li>
               <li>
                 <ul className="space-x-20 content-center">
                   <li className="flex text-[#079D0D] text-sm mt-1">
@@ -53,9 +53,9 @@ export default function pastpoll() {
               </li>
             </ul>
 
-            <ul className="max-w-2xl mx-auto p-2  rounded-md border border-[#e5e7eb] cursor-pointer mt-4">
+            <ul className="max-w-2xl mx-auto p-4 rounded-md border border-[#e5e7eb] cursor-pointer mt-4">
               <li className="text-base text-[#0A0A2F] mobile:text-lg font-bold">Others Election</li>
-              <li className="text-right mobile:text-left mobile:text-xs text-sm py-1">User Voter : 1950</li>
+              <li className="text-right mobile:text-left mobile:text-xs text-sm">User Voter : 1950</li>
               <li>
                 <ul className="space-x-20 content-center">
                   <li className="flex text-[#079D0D] text-sm mt-1">
