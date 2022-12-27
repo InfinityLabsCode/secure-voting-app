bb7dde8b1b4906e374a599be187f64c0a910d9f3
nahid-hasan <nahid.hasan3194@gmail.com>
2022-12-27 09:05:26 +0600

    added modal box

--- pages/pastpoll.tsx
+++ pages/pastpoll.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import Footer from '../components/Footer/index';
 import Header from '../components/Header/index';
-export default function pastpoll() {
+export default function Pastpoll() {
+  const [showModal, setShowModal] = useState(false);
   return (
     <>
       <Header />
@@ -11,10 +13,15 @@ export default function pastpoll() {
               <h1 className="text-2xl font-bold leading-7 mobile:text-xl mobile:truncate text-[#0A0A2F] ">
                 Past Polls
               </h1>
-              <p className="mt-1 text-sm text-[#0A0A2F]">Below are the Past Polls create by the administrators:</p>
+              <p className="mt-1 text-sm mobile:text-xs text-[#0A0A2F]">
+                Below are the Past Polls create by the administrators:
+              </p>
             </div>
 
-            <ul className="max-w-2xl mx-auto p-4  rounded-md border border-[#e5e7eb] cursor-pointer">
+            <ul
+              className="max-w-2xl mx-auto p-4  rounded-md border border-[#e5e7eb] cursor-pointer"
+              onClick={() => setShowModal(true)}
+            >
               <li className="text-base text-[#0A0A2F] mobile:text-lg font-bold">CEO Election</li>
               <li className="text-right mobile:text-left mobile:text-xs text-sm">User Voter : 1205</li>
               <li>
@@ -27,7 +34,10 @@ export default function pastpoll() {
               </li>
             </ul>
 
-            <ul className="max-w-2xl mx-auto p-4 rounded-md border border-[#e5e7eb] cursor-pointer mt-4">
+            <ul
+              className="max-w-2xl mx-auto p-4 rounded-md border border-[#e5e7eb] cursor-pointer mt-4"
+              onClick={() => setShowModal(true)}
+            >
               <li className="text-base text-[#0A0A2F] mobile:text-lg font-bold">Manager Election</li>
               <li className="text-right mobile:text-left mobile:text-xs text-sm">User Voter : 1250</li>
               <li>
@@ -40,7 +50,10 @@ export default function pastpoll() {
               </li>
             </ul>
 
-            <ul className="max-w-2xl mx-auto p-4 rounded-md border border-[#e5e7eb] cursor-pointer mt-4">
+            <ul
+              className="max-w-2xl mx-auto p-4 rounded-md border border-[#e5e7eb] cursor-pointer mt-4"
+              onClick={() => setShowModal(true)}
+            >
               <li className="text-base text-[#0A0A2F] mobile:text-lg font-bold">MD Election</li>
               <li className="text-right mobile:text-left mobile:text-xs text-sm">User Voter : 1850</li>
               <li>
@@ -53,7 +66,10 @@ export default function pastpoll() {
               </li>
             </ul>
 
-            <ul className="max-w-2xl mx-auto p-4 rounded-md border border-[#e5e7eb] cursor-pointer mt-4">
+            <ul
+              className="max-w-2xl mx-auto p-4 rounded-md border border-[#e5e7eb] cursor-pointer mt-4"
+              onClick={() => setShowModal(true)}
+            >
               <li className="text-base text-[#0A0A2F] mobile:text-lg font-bold">Others Election</li>
               <li className="text-right mobile:text-left mobile:text-xs text-sm">User Voter : 1950</li>
               <li>
@@ -66,7 +82,10 @@ export default function pastpoll() {
               </li>
             </ul>
 
-            <ul className="max-w-2xl mx-auto p-4 rounded-md border border-[#e5e7eb] cursor-pointer mt-4">
+            <ul
+              className="max-w-2xl mx-auto p-4 rounded-md border border-[#e5e7eb] cursor-pointer mt-4"
+              onClick={() => setShowModal(true)}
+            >
               <li className="text-base text-[#0A0A2F] mobile:text-lg font-bold">Others Election</li>
               <li className="text-right mobile:text-left mobile:text-xs text-sm">User Voter : 1950</li>
               <li>
@@ -82,6 +101,65 @@ export default function pastpoll() {
         </div>
       </div>
       <Footer />
+
+      {showModal ? (
+        <>
+          <div className="fixed inset-0 z-50 overflow-y-auto">
+            <div className="fixed inset-0 w-full h-full bg-black opacity-40" onClick={() => setShowModal(false)}></div>
+            <div className="flex items-center min-h-screen px-4 py-8">
+              <div className="w-full max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg bg-white">
+                <div className="flex justify-between border-b">
+                  <div className="modal-title mb-4">
+                    <h1 className="text-xl font-bold leading-7 mobile:text-base mobile:truncate text-[#0A0A2F] ">
+                      CEO Election
+                    </h1>
+                    <p className="mt-1 text-sm mobile:text-xs text-[#0A0A2F]">
+                      The Employees will choose their CEO for the company:
+                    </p>
+                  </div>
+                  <div className="modal-closs font-bold">
+                    <button className="w-full mt-1 text-gray-800 text-right" onClick={() => setShowModal(false)}>
+                      X
+                    </button>
+                  </div>
+                </div>
+                <div className="pt-4 pb-2 flex-auto justify-center">
+                  <div className="modal-body">
+                    <p className="mb-3">Proposals : </p>
+                    <ul className="list-decimal pl-5">
+                      <li>
+                        <ul className="flex flex-auto justify-between">
+                          <li>Nahid</li>
+                          <li>
+                            <button type="button" className="text-red-900 border rounded px-4 pt-1 pb-0.5">
+                              Vote
+                            </button>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <ul className="flex flex-auto justify-between mt-5">
+                          <li>Imran</li>
+                          <li>
+                            <button type="button" className="text-red-900 border rounded px-4 pt-1 pb-0.5">
+                              Vote
+                            </button>
+                          </li>
+                        </ul>
+                      </li>
+                    </ul>
+                    <div className="total-voted mt-6">
+                      <p>
+                        User Voted : <span>1020</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : null}
     </>
   );
 }
