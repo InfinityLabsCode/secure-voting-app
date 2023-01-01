f18ec22b64021b6ace5d6cfe88ce478c5b50e0f6
nahid-hasan <nahid.hasan3194@gmail.com>
2023-01-01 20:39:37 +0600

    update

--- pages/pastpoll.tsx
+++ pages/pastpoll.tsx
@@ -1,20 +1,19 @@
-
 //Dependencies
-import React,{ useState,useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 //Store
 import secureVoteStore from '../stores';
 
 //Componenets
+import Popup from '../components/ChoosePopup/popup';
+import Result from '../components/Result/result';
 import Footer from '../components/Footer/index';
 import Header from '../components/Header/index';
-import VotingPopup from '../components/VoteModal/modal';
-import Popup from '../components/ChoosePopup/popup';
-import { ISinglePoll } from '../models/allModels';
 import Loader from '../components/Loader';
-
-
+import { ISinglePoll } from '../models/allModels';
+import Acces from '../components/Acces/acces';
+import Connect from '../components/Connect/connect';
 
 export default function Pastpoll() {
   const router = useRouter();
@@ -25,30 +24,29 @@ export default function Pastpoll() {
   const isLoading = secureVoteStore((state) => state.uiStore.isLoading);
   const setIsShowEndingPopup = secureVoteStore((state) => state.uiStore.setIsShowEndingPopup);
 
+  useEffect(() => {
+    if (!isLogin) {
+      router.push('login');
+    }
+  }, [isLogin, router]);
 
-
-	useEffect(
-		() => {
-			if (!isLogin) {
-				router.push('login');
-			}
-		},
-		[ isLogin, router ]
-	);
-
-  useEffect(()=>{
-    (async()=>{
-      if(isLogin){
+  useEffect(() => {
+    (async () => {
+      if (isLogin) {
         setIsLoading(true);
         await getAllPollList();
         setIsLoading(false);
       }
     })();
-  },[isLogin,setIsLoading,getAllPollList]);
+  }, [isLogin, setIsLoading, getAllPollList]);
 
   return (
     <>
       <Header />
+      <Result />
+      <Acces />
+      <Connect />
+      
       <div className="flex-auto">
         <div className="max-w-3xl mx-auto pt-10 pb-12 px-8">
           <div className="bg-white mt-8 mobile:p-6 p-5 border-t-4 border-orange-300 overflow-visible rounded-md border-x border border-[#e5e7eb] border-t-[#0a0a2f]">
@@ -61,20 +59,28 @@ export default function Pastpoll() {
               </p>
             </div>
 
-            {!isLoading && pastPolls.length && pastPolls.map((item: ISinglePoll, index: number) => (
-              <ul onClick={() => setIsShowEndingPopup(true)} key={index} className="max-w-2xl mx-auto p-4  rounded-md border border-[#e5e7eb] cursor-pointer">
-                <li className="text-base text-[#0A0A2F] mobile:text-lg font-bold">{item?.name}</li>
-                <li className="text-right mobile:text-left mobile:text-xs text-sm">User Voter : {item?.voteCounted}</li>
-                <li>
-                  <ul className="space-x-20 content-center">
-                    <li className="flex text-[#079D0D] text-sm mt-1">
-                      <span className="w-2 h-2 mt-1 mr-2 rounded-full bg-[#079D0D]"></span>
-                      Completed
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            ))}
+            {!isLoading &&
+              pastPolls.length &&
+              pastPolls.map((item: ISinglePoll, index: number) => (
+                <ul
+                  onClick={() => setIsShowEndingPopup(true)}
+                  key={index}
+                  className="max-w-2xl mx-auto p-4  rounded-md border border-[#e5e7eb] cursor-pointer"
+                >
+                  <li className="text-base text-[#0A0A2F] mobile:text-lg font-bold">{item?.name}</li>
+                  <li className="text-right mobile:text-left mobile:text-xs text-sm">
+                    User Voter : {item?.voteCounted}
+                  </li>
+                  <li>
+                    <ul className="space-x-20 content-center">
+                      <li className="flex text-[#079D0D] text-sm mt-1">
+                        <span className="w-2 h-2 mt-1 mr-2 rounded-full bg-[#079D0D]"></span>
+                        Completed
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              ))}
 
             {!isLoading && !pastPolls.length ? <span>No past poll available!</span> : ''}
           </div>
