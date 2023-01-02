81837a66d09fcc0986062dd508213de26a2d4ad8
nahid-hasan <nahid.hasan3194@gmail.com>
2023-01-02 10:30:26 +0600

    update your bugfixes

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,9 +1,9 @@
 const index = () => {
   return (
     <footer className="bg-gray-100 left-0 bottom-0 right-0 absolute">
-      <div className="mt-4 border-t border-gray-300 py-8">
-        <h3 className="text-2xl text-center">SecurePoll</h3>
-        <p className="text-base text-center mt-2">© 2022 SecurePoll. All rights reserved.</p>
+      <div className="border-t border-gray-300 py-8">
+        <h3 className="text-2xl text-center">SecureVote</h3>
+        <p className="text-base text-center mt-2">© 2022 SecureVote. All rights reserved.</p>
         <p className="text-sm text-center">Making it easy to create instant, real-time polls and surveys for free.</p>
       </div>
     </footer>
